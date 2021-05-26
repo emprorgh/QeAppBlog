354d0365ccc2ba46fd1f499b92f2fb778351fabc
Sujan Ghimire <sujangh10@gmail.com>
2021-05-26 13:42:01 -0400

    added register form

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -9,6 +10,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { SdetComponent } from './sdet/sdet.component';
 import { AgileAutomationComponent } from './agile-automation/agile-automation.component';
+import { DownloadResourcesComponent } from './download-resources/download-resources.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { AgileAutomationComponent } from './agile-automation/agile-automation.co
     NavMenuComponent,
     HomeComponent,
     SdetComponent,
-    AgileAutomationComponent
+    AgileAutomationComponent,
+    DownloadResourcesComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +31,10 @@ import { AgileAutomationComponent } from './agile-automation/agile-automation.co
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'sdet', component: SdetComponent },
       { path: 'agile-automation', component: AgileAutomationComponent },
-    ])
+      { path: 'download-resources', component: DownloadResourcesComponent },
+      { path: 'registration', component: RegistrationComponent },
+    ]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ClientApp/src/app/download-resources/download-resources.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-download-resources',
+  templateUrl: './download-resources.component.html',
+  styleUrls: ['./download-resources.component.css']
+})
+export class DownloadResourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
